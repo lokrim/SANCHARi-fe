49448f01013730518fb4143361a642a0aee15d03
lokrim <ahmedzyed123@gmail.com>
2025-06-25 00:47:47 +0530

    Boilerplate code

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About SANCHARI</h1>
+      <p>This platform helps you collaboratively edit maps from satellite imagery.</p>
+    </div>
+  );
+}
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Your Dashboard</h1>
+      <p>Coming soon: data visualizations and edit history.</p>
+    </div>
+  );
+}
